2281d3eade04a3477b4b5c20a9c42d293d8a6ce2
MatheusRubens <matheuz.rubenz10@gmail.com>
2017-04-25 22:44:42 +0300

    Create exemplo11.php

---
+++ exemplo11.php
@@ -0,0 +1,17 @@
+<html>
+ <head>
+   <meta charset='utf-8'/>
+ </head>
+ <body>
+  <h1>ESTRUTURA DE REPETIÇÃO(DO WHILE)</h1>
+    <?php
+      $num=10;
+      do{
+         echo"numero:$num<br>";
+         $num--;
+        }
+        while($num>=1)
+      ?>
+     </body>
+  </html>  
+   
