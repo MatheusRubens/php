c081f29be6ebf7cf2786ff3b7bca674488ceb9bb
MatheusRubens <matheuz.rubenz10@gmail.com>
2017-04-25 22:42:27 +0300

    Create exemplo9.php

---
+++ exemplo9.php
@@ -0,0 +1,13 @@
+<html>
+ <head>
+   <meta charset="utf-8" >
+ </head>
+ <body>
+   <h1>ESTRUTURA DE REPETIÇÃO(FOR)</h1>
+       <?php
+          for($i=1;$i<=10;$i++){
+          echo"<b> numero:$i<br>";
+          }
+       ?>
+</body>
+</html>         
